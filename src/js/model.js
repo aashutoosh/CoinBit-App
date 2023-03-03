b465394d703e017bc80700d04f74ce36c95fda6e
Ashutosh <porwal0403@gmail.com>
2023-03-03 20:31:31 +0530

    Fixed: Discord alert bug

--- src/js/model.js
+++ src/js/model.js
@@ -182,35 +182,46 @@ export function sendDiscordAlert(alert, notfCallback) {
     const webhookURL = state.discordWebhookUrl;
     const discordEnabled = state.sendDiscordAlerts;
 
-    if (discordEnabled && typeof (webhookURL) === 'string' && webhookURL !== "") {
-        const payload = {
-            embeds: [
-                {
-                    type: "rich",
-                    title: alert.title,
-                    description: alert.description,
-                    color: 0xf0ba09,
-                    fields: [
-                        {
-                            name: "\u200B",
-                            value: `${alert.symbol} ${alert.condition} ${alert.price}`
+    if (discordEnabled) {
+        if (typeof (webhookURL) === 'string' && webhookURL !== "") {
+            const payload = {
+                embeds: [
+                    {
+                        type: "rich",
+                        title: alert.title,
+                        description: alert.description,
+                        color: 0xf0ba09,
+                        fields: [
+                            {
+                                name: "\u200B",
+                                value: `${alert.symbol} ${alert.condition} ${alert.price}`
+                            }
+                        ],
+                        footer: {
+                            text: DISCORD_FOOTER_TEXT
                         }
-                    ],
-                    footer: {
-                        text: DISCORD_FOOTER_TEXT
                     }
-                }
-            ]
-        };
+                ]
+            };
 
-        fetch(webhookURL, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(payload)
-        }).catch(error => notfCallback(`Error: ${error}`, 'ri-error-warning-line'));
-    }
-    else {
-        notfCallback('Please enter valid discord webhook URL.', 'ri-error-warning-line');
+            try {
+                fetch(webhookURL, {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify(payload)
+                }).then(response => {
+                    if (response.status === 405) {
+                        notfCallback(`Discord Webhook URL not valid`, 'ri-error-warning-line')
+                    }
+                }).catch(error => notfCallback(`Error: ${error}`, 'ri-error-warning-line'));
+            }
+            catch (error) {
+                notfCallback(`Error: ${error.message}`, 'ri-error-warning-line')
+            }
+        }
+        else {
+            notfCallback('Please enter valid discord webhook URL.', 'ri-error-warning-line');
+        }
     }
 }
 
